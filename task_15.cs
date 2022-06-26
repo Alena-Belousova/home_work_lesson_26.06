cfb257c1e91dced613a8797f7e73205a1f0fae7c
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-06-26 21:38:05 +0300

    сдача задачи 15

---
+++ task_15.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("задача 15");
+Console.Write("введите номер дня недели: ");//6
+int day = Convert.ToInt32(Console.ReadLine());  
+   
+       if(day>0 && day<6) {
+       Console.WriteLine ("нет");
+   }
+   else   {
+       if (day< 8)  {
+       Console.WriteLine ("да");  
+     }
+     else {Console.WriteLine ("ошибка");
+     }
+   }
+   
