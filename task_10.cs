11fa036feb5fd8b7be6212dbb535e344beffc066
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-06-26 21:34:07 +0300

    задача 10

---
+++ task_10.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("задача 10");
+Console.Write("введите трехзначное число: ");//123
+int numb = Convert.ToInt32(Console.ReadLine());
+int FirstDigit = numb/10;//123/10=12
+int SecondDigit= FirstDigit % 10;//12/10=1.2 (остаток от деления 2)
+Console.WriteLine("вторая цифра ="+ SecondDigit);
